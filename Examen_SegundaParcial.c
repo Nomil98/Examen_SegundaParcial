399ae57d75ff5560dd939ee748448142d264e226
Nomil98 <47511927+Nomil98@users.noreply.github.com>
2019-05-02 15:05:14 -0500

    Update Examen_SegundaParcial.c
    Anexo de la parte de la cadena de palabras

--- Examen_SegundaParcial.c
+++ Examen_SegundaParcial.c
@@ -5,6 +5,8 @@
 #fuses NOPBADEN, STVREN, NOLVP, NODEBUG
 #use delay(clock=16000000)
 
+
+
 int Led1=0, contadorled=0;
 #INT_TIMER0
 void TIMER0(){
@@ -17,13 +19,58 @@ void main(){
  setup_oscillator(OSC_16MHZ);
  set_tris_c(0x00);
  set_tris_b(0x00);
+ int inicio=0;
+char palabra[10]=getc(); //enter = 13, espacio =32
+char cadena[20];
+int num=0;
+int i=0;
+int j=0;
+int a=0;
  
 setup_timer_0(RTCC_INTERNAL |RTCC_DIV_128 );
-enable_interrupts(INT_TIMER0);//SE HABILITA LA INTERRUPCIÓN NECESARIA
+enable_interrupts(INT_TIMER0);//SE HABILITA LA INTERRUPCIÃ“N NECESARIA
 enable_interrupts(GLOBAL); // SE HABILITAN LAS INTERRUPCIONES GLOBALES
 set_timer0(122);
-   
 while(1){
+
+if (kbhit()){
+   if(cadena[i]>=65 && cadena[i]<=122 || cadena == 13)
+   {
+       printf("%c", cadena[i])
+       if(cadena[i]==13)
+       {
+         inicio =1;
+       }
+       i++;
+   }
+
+}
+
+if(inicio==1){
+   while( cadena[i]!= 32 && cadena[i]!=13){
+     palabra[j]=cadena[i];
+     j++;
+     i++;
+       
+  
+     }
+     i++;
+     while(cadena[i]!=13){
+     num = cadena[i];
+     
+     }
+   
+   printf("%c", palabra[j]);
+   printf("%d", num);
+  }
+  cadena[i]=0;
+  palabra[j]=0;
+  a=0;
+  num=0;
+  inicio=0;
+
+
+
 
 if(contadorled==1){
     output_b(Led1);
